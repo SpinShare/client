2aa11595996fe9d31295f73142f8430d74295d4c
Andreas Heimann <andreas@iklikla.de>
2020-03-31 20:37:46 +0200

    add ping, ads, new/popular songs api calls

---
+++ src/assets/js/api.js
@@ -0,0 +1,74 @@
+const axios = require('axios');
+
+class SHAPI {
+    constructor() {
+        this.apiBase = "http://localhost/www/customspeens-server/public/index.php/api/";
+        this.supportedVersion = 1;
+    }
+
+    async ping() {
+        let apiPath = this.apiBase + "ping";
+        let supportedVersion = this.supportedVersion;
+
+        return axios.get(apiPath)
+        .then(function(response) {
+            if(response.data.version !== supportedVersion) {
+                throw new Error("Client is outdated!");
+            }
+
+            return response.data.data;
+        }).catch(function(error) {
+            throw new Error(error);
+        });
+    }
+
+    async getAds() {
+        let apiPath = this.apiBase + "ads";
+        let supportedVersion = this.supportedVersion;
+
+        return axios.get(apiPath)
+        .then(function(response) {
+            if(response.data.version !== supportedVersion) {
+                throw new Error("Client is outdated!");
+            }
+            
+            return response.data.data;
+        }).catch(function(error) {
+            throw new Error(error);
+        });
+    }
+
+    async getNewSongs(_offset) {
+        let apiPath = this.apiBase + "songs/new/" + _offset;
+        let supportedVersion = this.supportedVersion;
+
+        return axios.get(apiPath)
+        .then(function(response) {
+            if(response.data.version !== supportedVersion) {
+                throw new Error("Client is outdated!");
+            }
+            
+            return response.data.data;
+        }).catch(function(error) {
+            throw new Error(error);
+        });
+    }
+
+    async getPopularSongs(_offset) {
+        let apiPath = this.apiBase + "songs/popular/" + _offset;
+        let supportedVersion = this.supportedVersion;
+
+        return axios.get(apiPath)
+        .then(function(response) {
+            if(response.data.version !== supportedVersion) {
+                throw new Error("Client is outdated!");
+            }
+            
+            return response.data.data;
+        }).catch(function(error) {
+            throw new Error(error);
+        });
+    }
+}
+
+module.exports = SHAPI;
