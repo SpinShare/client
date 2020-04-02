653c48949530f92ba9eb52c5e2cf21c1cf07fa10
Andreas Heimann <andreas@iklikla.de>
2020-04-02 14:43:52 +0200

    added user settings, added localization

--- src/assets/js/init.js
+++ src/assets/js/init.js
@@ -1,11 +1,40 @@
 const { ipcRenderer } = require('electron');
 const { dialog, shell, app } = require('electron').remote;
+const isDev = require('electron-is-dev');
 const path = require('path');
 const SSAPI = require( path.resolve(__dirname, './assets/js/module.api.js') );
 const SRXD = require( path.resolve(__dirname, './assets/js/module.srxd.js') );
+const UserSettings = require( path.resolve(__dirname, './assets/js/module.usersettings.js') );
+const Locale = require( path.resolve(__dirname, './assets/js/module.locale.js') );
 const fs = require('fs');
 const ncp = require('ncp');
 const http = require('http');
 
+let systemOS = process.platform;
+let tempDirLocation = app.getPath('temp');
+
 let api = new SSAPI();
 let srxdControl = new SRXD();
+
+// TODO: Mac/Linux Support
+function detectGameDirectory() {
+    if(process.platform == "win32") {
+        return path.join(app.getPath("userData"), "../..", "LocalLow", "Super Spin Digital", "Spin Rhythm XD", "Custom");
+    } else {
+        console.error("Unsupported system");
+    }
+}
+
+let userSettings = new UserSettings({
+    defaults: {
+        showExplicit: false,
+        gameDirectory: detectGameDirectory(),
+        language: app.getLocale()
+    }
+});
+
+let locale = new Locale(userSettings.get('language'));
+const DOMLocaleElements = document.querySelectorAll("*[locale]");
+DOMLocaleElements.forEach(function(localeElement) {
+    localeElement.innerHTML = locale.get(localeElement.innerHTML);
+});
