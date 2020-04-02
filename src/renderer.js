878d67129bfe361d22378abafa455d7c88fc5278
jy1263 <50583248+jy1263@users.noreply.github.com>
2020-04-02 17:35:43 +1100

    Delete renderer.js

--- src/renderer.js
+++
@@ -1,8 +0,0 @@
-document.ondragover = document.ondrop = (ev) => {
-    ev.preventDefault()
-  }
-  
-  document.body.ondrop = (ev) => {
-    console.log(ev.dataTransfer.files[0].path)
-    ev.preventDefault()
-  }
